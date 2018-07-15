cc49cf6c61ca2a8a95e222830bd1b4624d44b5f7
Jose Manuel Delgado Trueba <delgadotrueba@outlook.es>
2018-07-16 00:26:15 +0200

    year

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public year = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit() {
